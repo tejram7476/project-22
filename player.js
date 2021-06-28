dbfbb861d927c2b55d36dfb073308ccde5bc3013
harshad tejram gahukar <85113453+tejram7476@users.noreply.github.com>
2021-06-28 13:03:30 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,14 @@
+class Player{
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+        this.spt=createSprite(this.x,this.y,50,50);
+        this.spt.shapeColor="orange";
+        this.spt.scale=0.05;
+        this.spt.addAnimation("player",playerAnimation);
+    }
+    move(xdir,ydir){
+        this.spt.x+= xdir*grid;
+        this.spt.y+= ydir*grid;
+    }
+}
